2182ad6459387daf84bef2a7ac8f64491e3fbd04
Miguel Angelo <140343469+M1gu3lDev@users.noreply.github.com>
2024-04-20 18:59:39 -0300

    Delete GameStore.java

--- GameStore.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface GameStore {
-    public boolean cadastrarJogo(Jogo jogo);
-    public boolean apagarJogoCadastrado(int id);
-    public List<Jogo> getJogos();
-    public List<Jogo> pesquisarJogosPorCategoria(String categoria);
-
-}
