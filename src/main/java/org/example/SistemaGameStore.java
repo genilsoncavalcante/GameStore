54b6e71d74c7e1f7d08c4378f91da4384db387f9
Miguel Angelo <140343469+M1gu3lDev@users.noreply.github.com>
2024-04-21 21:55:29 -0300

    Update SistemaGameStore.java

--- src/main/java/org/example/SistemaGameStore.java
+++ src/main/java/org/example/SistemaGameStore.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class SistemaGameStore {
     public static void main(String[] args) {
-        GameStore meusJogos = new JogoList();
+        JogoList meusJogos = new JogoList();
         Jogo resident_evil = new Jogo(1, "Resident Evil", "10/01/2005", "Terror", 4.7, 97.88, 20.0);
 
         //teste de verificação
